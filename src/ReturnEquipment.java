749139256808207a5fe6a82d97d4b051077f170a
Vitor Marcato <vitor.marcatof@gmail.com>
2022-07-31 20:44:22 -0300

    update ReturnEquipment and add SchoolEquipment classes

--- src/ReturnEquipment.java
+++ src/ReturnEquipment.java
@@ -1,8 +1,33 @@
 import java.util.Calendar;
 
 public class ReturnEquipment {
-	String UserName;
-	Calendar returnDate;
-	String ClassId;
+	String userName;
+	Calendar checkInDate;
+	String classId;
+	boolean damageStatus;
 	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Calendar getCheckInDate() {
+		return checkInDate;
+	}
+	public void setCheckInDate(Calendar checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+	public String getClassId() {
+		return classId;
+	}
+	public void setClassId(String classId) {
+		this.classId = classId;
+	}
+	public boolean isDamageStatus() {
+		return damageStatus;
+	}
+	public void setDamageStatus(boolean damageStatus) {
+		this.damageStatus = damageStatus;
+	}
 }
---
+++ src/SchoolEquipment.java
@@ -0,0 +1,33 @@
+
+public class SchoolEquipment {
+	String equipmentName;
+	String equipmentDescription;
+	int equipmentId;
+	String equipmentType;
+	
+	public String getEquipmentName() {
+		return equipmentName;
+	}
+	public void setEquipmentName(String equipmentName) {
+		this.equipmentName = equipmentName;
+	}
+	public String getEquipmentDescription() {
+		return equipmentDescription;
+	}
+	public void setEquipmentDescription(String equipmentDescription) {
+		this.equipmentDescription = equipmentDescription;
+	}
+	public int getEquipmentId() {
+		return equipmentId;
+	}
+	public void setEquipmentId(int equipmentId) {
+		this.equipmentId = equipmentId;
+	}
+	public String getEquipmentType() {
+		return equipmentType;
+	}
+	public void setEquipmentType(String equipmentType) {
+		this.equipmentType = equipmentType;
+	}
+	
+}
