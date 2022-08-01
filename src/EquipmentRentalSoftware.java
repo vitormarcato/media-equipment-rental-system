319e49949bc78fb398eecc8b16ed61de0e37b85b
Vitor Marcato <vitor.marcatof@gmail.com>
2022-07-31 21:02:59 -0300

    Add an interface prototype

--- src/EquipmentRentalSoftware.java
+++ src/EquipmentRentalSoftware.java
@@ -2,10 +2,14 @@
 public class EquipmentRentalSoftware {
 
 	public static void main(String[] args) {
-		
-		RentEquipment rent = new RentEquipment();
-		System.out.println(rent);
-
+		System.out.println("____________________________________________");
+		System.out.println("# Welcome to Media Equipment Rental System #");
+		System.out.println("____________________________________________");
+		System.out.println("# Please, choose an option #");
+		System.out.println("1 - show equipment list");
+		System.out.println("2 - order equipment");
+		System.out.println("3 - return equipment");
+		System.out.println("4 - register new equipment");
 	}
 
 }
