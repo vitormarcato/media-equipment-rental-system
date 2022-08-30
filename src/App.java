eee6b273a5b3f7bb8ce1b859d8d4e197a4b2e1d0
Vitor Marcato <vitor.marcatof@gmail.com>
2022-08-30 14:40:10 -0300

    refactor: update App class with switch case

--- src/App.java
+++ src/App.java
@@ -10,31 +10,51 @@ public class App {
 		PrintFirstScreen.welcomeDisplay();
 		userInicialInput = Integer.parseInt(sc.nextLine());
 		
-		if (userInicialInput == 1) {
-			RegisterNewEquipment optionOne = new RegisterNewEquipment();
-			optionOne.registerNewEquipment();
-			optionOne.getListItensOfIventory();
-			optionOne.displayInventory();
-			
-		} else if (userInicialInput == 2) {
-			BookEquipment optionTwo = new BookEquipment();
-			optionTwo.equipmentBooked();
-			
-		} else if (userInicialInput == 3) {
-			ReturnEquipment optionThree = new ReturnEquipment();
-			optionThree.returnEquipment();			
-
-		} else if (userInicialInput == 4) {
-			
-			RegisterNewEquipment optionFour = new RegisterNewEquipment();
-			optionFour.getListItensOfIventory();
-			optionFour.displayInventory();			
-			
-		} else if (userInicialInput == 0) {
-			System.exit(0);
-		} else {
-			System.out.println("Please, try again.");
+		switch (userInicialInput) {	
+		case 1: RegisterNewEquipment optionOne = new RegisterNewEquipment();
+		optionOne.registerNewEquipment();
+		optionOne.getListItensOfIventory();
+		optionOne.displayInventory();
+		break;
+		case 2: BookEquipment optionTwo = new BookEquipment();
+		optionTwo.equipmentBooked();
+		break;
+		case 3:	ReturnEquipment optionThree = new ReturnEquipment();
+		optionThree.returnEquipment();			
+		break;
+		case 4:	RegisterNewEquipment optionFour = new RegisterNewEquipment();
+		optionFour.getListItensOfIventory();
+		optionFour.displayInventory();		
+		break;
+		case 0: System.exit(0);
+		default: System.out.println("Please, try again.");
 		}
+			
+//		if (userInicialInput == 1) {
+//			RegisterNewEquipment optionOne = new RegisterNewEquipment();
+//			optionOne.registerNewEquipment();
+//			optionOne.getListItensOfIventory();
+//			optionOne.displayInventory();
+//			
+//		} else if (userInicialInput == 2) {
+//			BookEquipment optionTwo = new BookEquipment();
+//			optionTwo.equipmentBooked();
+//			
+//		} else if (userInicialInput == 3) {
+//			ReturnEquipment optionThree = new ReturnEquipment();
+//			optionThree.returnEquipment();			
+//
+//		} else if (userInicialInput == 4) {
+//			
+//			RegisterNewEquipment optionFour = new RegisterNewEquipment();
+//			optionFour.getListItensOfIventory();
+//			optionFour.displayInventory();			
+//			
+//		} else if (userInicialInput == 0) {
+//			System.exit(0);
+//		} else {
+//			System.out.println("Please, try again.");
+//		}
 		sc.close();
 	}
 }
